e378ba3cc310d511c8e56ce1ab527f5498a9271d
aryanpurohit77 <119164558+aryanpurohit77@users.noreply.github.com>
2024-02-10 16:04:35 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: ["Mulish" , "sans-serif"],
+      },
+      colors:{
+        deepblue:"#02042a",
+        lightblue:"#2b84ea",
+        lightblue300:"#4b94ed",
+        lightblue500:"#0b72e7",
+        greenLight:"#61cea6",
+        grayText:"#818597",
+        lightGray:"#e2e2e2",
+        grayblue:"#344a6c",
+        deepBlueHead:"#162f56",
+        gray2:"#525a76",
+        backg:'#191d2e',
+        features:'#1d6bc0',
+      },
+    },
+  },
+  plugins: [],
+}
